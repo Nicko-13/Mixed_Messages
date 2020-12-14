d0ff95b97dc40a54ee8334f93f9bf16afdca6272
Nicko-13 <schyokinn.n@gmail.com>
2020-12-14 15:42:46 +0300

    App developed

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const noun = ['I', 'We', 'You', 'They'];
 const verb = ['love', 'adore', 'keen on', 'interesting in']
 const subject = ['coding', 'learning', 'chatting', 'laughing']
 
+/**
+ * Return random message combined from given arrays
+ */
 const get_random_message = () => {
     console.log([noun, verb, subject]
-                .reduce()
-                .join() + '!')
+                .map(words => get_random_item(words))
+                .join(' ') + '!'
+                )
 }
-
+/**
+ * Return random element from the array
+ * 
+ * @param {Array} array
+ */
 const get_random_item = array => {
     return array[Math.floor(Math.random() * array.length)]
 }
